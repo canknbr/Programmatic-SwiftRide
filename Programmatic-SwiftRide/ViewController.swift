aa4313cfa4480508a990ae30a6b73dd8f1dc1d73
canknbr <cankanbur1@gmail.com>
2023-06-05 18:17:57 +0300

    Initial Commit

---
+++ Programmatic-SwiftRide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-SwiftRide
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
